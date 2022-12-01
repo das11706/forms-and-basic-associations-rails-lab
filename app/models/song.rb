27830184d7a0712fa231136dfae2cf66d2dbf703
Daniel Suarez <suarez.daniel101@gmail.com>
2022-11-30 21:00:27 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,19 +12,11 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  # def genre_name=(name)
-  #   self.genre = Genre.find_by(name: name)
-  # end
-
-  # def genre_name
-  #   self.genre ? self.genre.name : nil
-  # end
-
-  def note_contents=(content)
-    
+  def note_contents=(contents)
+    self.notes = contents.map {|n| Note.new(:content => n) }
   end
 
   def note_contents
-
+    self.notes 
   end
 end
